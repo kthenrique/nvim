c20daa16a1906ec0d9fef2d01c19e8b87a57a071
Torres Henrique, Kelve <kelve.torreshenrique@elektrobit.com>
2025-04-28 10:11:03 +0200

    Exp with Octo

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -160,6 +160,19 @@ local plugins = {
 			require("overseer").setup()
 		end,
 	},
+	{
+		"pwntester/octo.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			-- OR 'ibhagwan/fzf-lua',
+			-- OR 'folke/snacks.nvim',
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("octo").setup()
+		end,
+	},
 }
 
 require("lazy").setup(plugins, opts)
