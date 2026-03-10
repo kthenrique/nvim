7257b07f3ba2573b49735fcb335b97bb91145fcf
Kelve T. Henrique <kthenrique@pm.me>
2026-03-10 15:56:17 +0100

    Simplify scripts for terminal toggler

--- lua/terminal.lua
+++ lua/terminal.lua
@@ -1,20 +1,11 @@
 ------------------------------------------------------------------------------------ FTERM
 local fterm = require("FTerm")
-fterm.setup({
-	cmd = function()
-		local sh = os.getenv("SHELL")
-		if sh == nil or sh == "" then
-			sh = "/bin/bash"
-		end
-		return sh
-	end,
-	-- Neovim's native window border. See `:h nvim_open_win` for more configuration options.
+local shell = os.getenv("SHELL") or "bash"
+local termOpts = { noremap = true, silent = true }
+
+local termSetup = {
+	cmd = shell,
 	border = "double",
-	-- Close the terminal as soon as shell/command exits.
-	-- Disabling this will mimic the native terminal behaviour.
-	auto_close = true,
-	-- Highlight group for the terminal. See `:h winhl`
-	hl = "Normal",
 	-- Transparency of the floating window. See `:h winblend`
 	blend = 20,
 	-- Object containing the terminal window dimensions.
@@ -25,57 +16,27 @@ fterm.setup({
 		x = 0.5, -- X axis of the terminal window
 		y = 0.5, -- Y axis of the terminal window
 	},
-	-- Callback invoked when the terminal exits.
-	-- See `:h jobstart-options`
-	on_exit = nil,
-	-- Callback invoked when the terminal emits stdout data.
-	-- See `:h jobstart-options`
-	on_stdout = nil,
-	-- Callback invoked when the terminal emits stderr data.
-	-- See `:h jobstart-options`
-	on_stderr = nil,
-})
+}
 
-vim.cmd('command! FTermOpen lua require("FTerm").open()')
--- This will close the terminal window but preserves the actual terminal session
-vim.cmd('command! FTermClose lua require("FTerm").close()')
--- Unlike closing, this will remove the terminal session
-vim.cmd('command! FTermExit lua require("FTerm").exit()')
--- Toggling the terminal
-vim.cmd('command! FTermToggle lua require("FTerm").toggle()')
+fterm.setup(termSetup)
+local terms = {
+	a = fterm,
+	b = fterm:new(termSetup),
+	c = fterm:new(termSetup),
+}
 
-vim.api.nvim_set_keymap(
-	"n",
-	"<C-b>",
-	'<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>',
-	{ noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-	"t",
-	"<C-b>",
-	'<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>',
-	{ noremap = true, silent = true }
-)
-
-local admin_term = fterm:new({
-	ft = "fterm_shell",
-	cmd = os.getenv("SHELL"),
-	blend = 10,
-	dimensions = {
-		height = 0.9,
-		width = 0.95,
-	},
-})
-
--- Use this to toggle btop in a floating terminal
-function fterm_shell_toggle()
-	admin_term:toggle()
+local function toggle_shell(id)
+	if vim.bo.buftype == "terminal" then
+		vim.cmd("stopinsert")
+	end
+	terms[id]:toggle()
 end
 
-vim.cmd("command! FTermShell lua fterm_shell_toggle()")
-vim.api.nvim_set_keymap("n", "<C-a>", "<C-\\><C-a><CMD>FTermShell<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("t", "<C-a>", "<C-\\><C-a><CMD>FTermShell<CR>", { noremap = true, silent = true })
-
+for key, _ in pairs(terms) do
+	vim.keymap.set({ "n", "t" }, "<A-" .. key .. ">", function()
+		toggle_shell(key)
+	end, termOpts)
+end
 ----------------------------------------------------------------------------------- GOMOVE
 require("gomove").setup({
 	-- whether or not to map default key bindings, (true/false)
